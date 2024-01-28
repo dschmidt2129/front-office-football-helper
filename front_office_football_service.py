e4b4d6854f505ce2c72cf6524bd1c91cc4566a80
David Schmidt <dschmidt2129@gmail.com>
2024-01-28 17:18:03 -0500

    adding team information logic so that player and roster info can be added in the future

--- front_office_football_service.py
+++ front_office_football_service.py
@@ -1,9 +1,43 @@
 import pandas as pd
 
 class FileReader:
+    # todo: need to completely refactor this class because it is completely muddled with too much varied functionality
     def __init__(self):
         # no variables need initialized
         pass
+
+    # function to return the team id from the team_information.csv file 
+    def get_team_info(self, team):
+        # cleveland is team id 30
+        # las vegas is team id 20
+        # miami is team id 14
+        # indianapolis is team id 11
+        # todo: update this file location to the game file location. 
+        # todo: may need to have the users upload this file at each run? not sure how often this file changes
+        team_info = pd.read_csv("resources/team_information.csv")
+        print(team_info)
+        team_id = -1
+        # switch statement to return team id
+        match team:
+            case 'Cleveland':
+                team_id = team_info.iat[30,0]
+                print('Cleveland team id: ' + str(team_id))
+                return team_id
+            case 'Las Vegas':
+                team_id = team_info.iat[20,0]
+                print('Las Vegas team id: ' + str(team_id))
+                return team_id
+            case 'Miami':
+                team_id = team_info.iat[14,0]
+                print('Miami team id: ' + str(team_id))
+                return team_id
+            case 'Indianapolis':
+                team_id = team_info.iat[11,0]
+                print('Indianapolis team id: ' + str(team_id))
+                return team_id
+            case _:
+                print("No team passed!!")
+                return team_id
 
     def get_game_log(self, file_name):
         # grabs the game log from the local game file system
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from front_office_football_service import FileReader
 
 scrape = FileReader()
-(scrape.get_player_name_from_play())
+# (scrape.get_player_name_from_play())
+scrape.get_team_info('Cleveland')
